1438a752d278eb72d3c526ee8f2eda8af0aa2aad
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-03-23 18:50:09 -0300

    test2

--- authuser/src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -14,10 +14,8 @@ public class ResolverConfig extends WebMvcConfigurationSupport {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new SpecificationArgumentResolver());
-
         PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
         argumentResolvers.add(resolver);
-
         super.addArgumentResolvers(argumentResolvers);
     }
 
